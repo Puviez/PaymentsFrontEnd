4e9a32c8a2d1a3b598ec89d3d07bcedd8d226935
Puviez <boredkeeper1793@gmail.com>
2020-02-28 05:26:12 +0800

    Created Transaction History

--- src/components/history.js
+++ src/components/history.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import Navbar from './navbar'
+import Moment from 'react-moment';
+import 'moment-timezone';
 
 class History extends React.Component {
+    constructor (props) {
+        super (props)
+        this.state = {
+            transactions: []
+        }
+    }
+
+    componentDidMount () {
+        fetch("http://localhost:3000/payments/" + this.props.currentUser._id)
+        .then(response => response.json())
+        .then(payments => {
+            console.log(payments)
+            this.setState({
+            transactions: payments
+            });
+        }); 
+        console.log(this.state)
+    }
     render () {
         return (
             <React.Fragment>
@@ -9,24 +29,24 @@ class History extends React.Component {
                     <thead>
                         <tr>
                             <th scope="col">Date</th>
-                            {/* <th scope="col">Store</th> */}
+                            <th scope="col">Store</th>
                             <th scope="col">Category</th>
                             <th scope="col">Amount</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* {this.props.currentUser.transactions.map(
+                        {this.state.transactions.map(
                             transaction => {
                                 return (
                                     <tr>
-                                        <td>{transaction.created}</td>
-                                        <td>{store}</td>
+                                        <td><Moment format="DD/MM/YYYY">{transaction.date}</Moment></td>
                                         <td>{transaction.description}</td>
+                                        <td>{transaction.category}</td>
                                         <td>{transaction.amount}</td>
                                     </tr>
                                 )
                             }
-                        )} */}
+                        )}
                     </tbody>
                 </table>
                 <Navbar toLogout={this.props.toLogout}/>
